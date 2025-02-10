1ac43133b77a2d9c894a83ee096ce58c00ab3aa7
alfaturachman <122202202929@mhs.dinus.ac.id>
2025-02-10 19:04:12 +0700

    initial commit

---
+++ app/src/main/java/com/example/iconnet/api/RetrofitClient.kt
@@ -0,0 +1,27 @@
+package com.example.iconnet.api
+
+import com.example.iconnet.api.ApiService
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private val ip: String = "192.168.100.16"
+
+    // Buat BASE_URL dengan menggabungkan string
+    private val BASE_URL = "http://$ip:80/iconnet_api/"
+
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    val instance: ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
+}
